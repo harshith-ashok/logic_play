23a9643961041faa02a511e568a8977651d93398
Harshith Ashok <harshith.7k10@gmail.com>
2026-07-20 22:09:54 +0530

    initial commit

---
+++ src/App.vue
@@ -0,0 +1,8 @@
+<script setup lang="ts"></script>
+
+<template>
+  <div class="justify-center flex flex-col gap-4 items-center h-screen">
+    <img src="./assets/vue.svg" alt="" />
+    <div class="font-mono">we cookin</div>
+  </div>
+</template>
